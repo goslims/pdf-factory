c6abeb33440d91fb2d7ee1bb615117ca27cf2089
Drajat Hasan <drajathasan20@gmail.com>
2023-10-06 21:33:32 +0700

    Add getter for CustomFields

--- src/Contract.php
+++ src/Contract.php
@@ -16,9 +16,61 @@ abstract class Contract
         $this->setPdf();
     }
 
+    /**
+     * PDF report in other place have
+     * different fields. So provide yours
+     * in this method.
+     *
+     * @return array
+     */
+    protected function getCustomFields():array
+    {
+        return [];
+    }
+
+    /**
+     * Set your PDF Core Engine instance
+     * to $pdf property
+     *
+     * @return void
+     */
     abstract protected function setPdf():void;
+
+    /**
+     * Prepare and setup your content in 
+     * this method. Create it and generate!
+     *
+     * @param array $data
+     * @return self
+     */
     abstract protected function setContent(array $data = []):self;
+
+    /**
+     * Generate PDF output and download it
+     * as file
+     *
+     * @param string $filname
+     * @return void
+     */
     abstract protected function download(string $filname):void;
+
+    /**
+     * Stream your PDF report on browser
+     * without download it
+     *
+     * @param string|null $filname
+     * @param array|null $options
+     * @return void
+     */
     abstract protected function stream(?string $filname = null, ?array $options = null):void;
+
+    /**
+     * Generate PDF to save it into
+     * file.
+     *
+     * @param string $filepath
+     * @param Closure|null $callback
+     * @return void
+     */
     abstract protected function saveToFile(string $filepath, ?Closure $callback = null):void;
 }
--- src/Factory.php
+++ src/Factory.php
@@ -2,9 +2,22 @@
 namespace SLiMS\Pdf;
 
 final class Factory {
+
+    /**
+     * Factory instance
+     */
     private static ?Factory $instance = null;
+
+    /**
+     * List of provider
+     */
     private array $providers = [];
+
+    /**
+     * PDF Provider instance
+     */
     private ?object $pdf = null;
+
     private array $providerCommands = [
         'setContent','download', 
         'stream', 'saveToFile'
@@ -18,11 +31,28 @@ final class Factory {
         return self::$instance;
     }
 
+    /**
+     * If you have yours don't forget to
+     * register it.
+     *
+     * @param string $name
+     * @param string $providerClass
+     * @return void
+     */
     public static function registerProvider(string $name, string $providerClass):void
     {
         self::getInstance()->providers[$name] = $providerClass;
     }
 
+    /**
+     * After you register your provider
+     * you need tell the factory to use you own
+     * as current provider to generate PDF
+     *
+     * @param string $providerName
+     * @param array $options
+     * @return void
+     */
     public static function useProvider(string $providerName, array $options = [])
     {
         $class = self::getInstance()->providers[$providerName]??null;
@@ -34,6 +64,16 @@ final class Factory {
         self::getInstance()->pdf = $providerInstance;
     }
 
+    /**
+     * SLiMS is not directly interacted with your provider,
+     * for modularity of this flow SLiMS is only interact
+     * with your provider through the Factory. Because of that
+     * your own provider must be extend factory contract.
+     *
+     * @param string $method
+     * @param array $arguments
+     * @return void
+     */
     public static function __callStatic(string $method, array $arguments)
     {
         if (in_array($method, self::getInstance()->providerCommands));
